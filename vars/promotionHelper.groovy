1e30a41640cc79fea160e0d3b54e00cf8f65470c
Leigh Gordon <leigh.gordon@utas.edu.au>
2020-04-27 17:25:36 +1000

    Add promotionHelper.getUpstreamTriggerOrLatestBuildNumber method

--- vars/promotionHelper.groovy
+++ vars/promotionHelper.groovy
@@ -1,11 +1,12 @@
 #!groovy
 
+import jenkins.model.Jenkins
 import hudson.model.Result
 import jenkins.model.CauseOfInterruption.UserInterruption
 import org.jenkinsci.plugins.workflow.support.steps.build.RunWrapper
 
 
-def abortPreviousBuilds(RunWrapper promotedBuild) {
+void abortPreviousBuilds(RunWrapper promotedBuild) {
     def previousBuild = promotedBuild.rawBuild.getPreviousBuild()
 
     while (previousBuild != null) {
@@ -20,4 +21,33 @@ def abortPreviousBuilds(RunWrapper promotedBuild) {
         }
         previousBuild = previousBuild.getPreviousBuild()
     }
+}
+
+/**
+ * Get the build number of the build which triggered a given build. If no upstreams are detected, retrieve the latest
+ * successful build instead
+ *
+ * @param promotedBuild the downstream build instance to be queried
+ * @param upstreamJobName the upstream build name
+ * @return the build number from an upstream job
+ */
+static int getUpstreamTriggerOrLatestBuildNumber(RunWrapper promotedBuild, String upstreamJobName) {
+    int buildNum
+
+    // if build is trigger from upstream, retrieve the build number of the build that triggered this build
+    promotedBuild.getUpstreamBuilds().each { b ->
+        buildNum = b.number
+        println(">> Automatic trigger detected, using build ${b.getFullDisplayName()}")
+        return buildNum
+    }
+
+    // if build is trigger manually by a user, there is no upstream, so get the latest build number
+    def job = Jenkins.instance.getJob(upstreamJobName)
+    def build = job?.lastSuccessfulBuild
+    if (build == null) {
+        throw new RuntimeException("No builds detected for job ${upstreamJobName}")
+    }
+    buildNum = build.number
+    println(">> Manual trigger detected, using latest build ${build.getFullDisplayName()}")
+    return buildNum
 }
